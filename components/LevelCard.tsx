f39cc8bee18b2775c6a280df07127c856e8730f6
mehdi-kada <mehdikada64@gmail.com>
2025-07-07 20:46:36 +0100

    fix/ levle card + sentery config

--- components/LevelCard.tsx
+++ components/LevelCard.tsx
@@ -31,19 +31,19 @@ function LevelCard({ level, title, description }: cardProps) {
   };
 
   return (
-    <div className="group relative bg-[#2F2F7F]/50 border border-white/10 p-6 rounded-xl overflow-hidden transition-all duration-300 ease-in-out hover:-translate-y-1.5 hover:border-[#E62136]">
+    <div className="group relative bg-[#2F2F7F]/50 border space-y-8 sm:space-y-0 border-white/10 p-6 rounded-xl overflow-hidden transition-all duration-300 ease-in-out hover:-translate-y-1.5 hover:border-[#E62136]">
       <div className="relative z-10 pointer-events-none ">
         <h3 className="text-4xl font-black text-[#E62136]">{level}</h3>
         <h4 className="font-bold text-2xl mb-2 mt-2 text-white">{title}</h4>
-        <p className="text-gray-400 mt-4 h-24 transition-opacity duration-300 group-hover:opacity-0">
+        <p className="text-gray-400 mt-4 h-24 transition-opacity duration-300 sm:group-hover:opacity-0">
           {description}
         </p>
       </div>
-      <div className="absolute inset-0 p-8 flex flex-col justify-end items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+      <div className="absolute inset-0 p-8 flex flex-col justify-end items-center opacity-100 sm:opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
         <div className="flex flex-col gap-4 w-full">
           <button
             onClick={() => handleSubmit()}
-            className="w-full text-center bg-[#E62136] hover:shadow-md hover:shadow-[#E62136]/30 hover:-translate-y-px transition-all duration-200 text-white font-bold py-3 px-4 rounded-lg"
+            className="w-full text-center  bg-[#E62136] hover:shadow-md hover:shadow-[#E62136]/30 hover:-translate-y-px transition-all duration-200 text-white font-bold py-3 px-4 rounded-lg"
           >
             Start Session
           </button>
